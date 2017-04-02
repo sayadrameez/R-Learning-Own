e595b887d3fbe4bd75f17d22430d05ac451fc169
sayadrameez <sayadrameez@gmail.com>
2017-04-02 15:41:22 +0400

    more functions added to test

--- Test.R
+++ Test.R
@@ -10,11 +10,42 @@ rm(z_v)
  checkEqualSign()
  
  genre_vector<-c("Action","Comedy","Thriller","Thriller","Action","Action")
- 
+ movielength_vector<- c("Short","Long","Medium","Medium","Long","Long")
+ year_vector <-c(1980,2002,2015,1992,2012,2008)
+ movies_vector<-c("Matrix","LOTR","Star Wars","Hobbit","Harry Potter","Home Alone")
+   
  factorSummaryCheck<-function(){
    print(factor(genre_vector ))
    print(summary(genre_vector))
    print(summary(factor(genre_vector)))
+   print(
+     factor(movielength_vector,ordered = TRUE, levels = c("Short","Medium","Long") )
+   )
+   
  }
  
  factorSummaryCheck()
+ 
+ vectorOperationsLength<- function(){
+   names(year_vector) <- movies_vector
+   print(movies_vector)
+   print(year_vector)
+   sorte_movies_vector<-sort(movies_vector)
+   sorte_year_vector<-sort(year_vector)
+   print(sorte_movies_vector)
+   print(sorte_year_vector)
+   print(min(movies_vector))
+   print(max(movies_vector))
+   print(min(year_vector))
+   print(max(year_vector))
+ }
+ 
+ vectorOperationsLength()
+ 
+ checkAverage <- function(){
+  sumdivbylen<-  sum(year_vector)/ length(year_vector)
+  print(sumdivbylen)
+  print(mean(year_vector))
+ }
+ 
+ checkAverage()
